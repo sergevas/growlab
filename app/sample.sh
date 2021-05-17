f7d4dbf51682e27dea4ae66edfd4a61a89e96625
sergevas <brighterr@gmail.com>
2021-05-18 02:53:16 +0300

    Update script: now creatres docs dir in the same repo

--- app/sample.sh
+++ app/sample.sh
@@ -3,12 +3,9 @@ python3 app.py
 
 export GIT_SSH_COMMAND="ssh -i `pwd`/.ssh/id_rsa"
 
-cp html/* ./growlab-live/docs/
-cd growlab-live
+cp html/* ../docs/
 
-git add .
-
+git add ../docs
 git commit -s -m "Update images at `date`"
 git pull origin master --rebase
 git push origin master
-
